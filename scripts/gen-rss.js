7a51482a9a00bd004914461b7974072a4e2eeaef
Andrea Bianchi <bianan96@gmail.com>
2023-01-08 23:11:03 +0100

    Personalize boilerplate code

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Andrea Bianchi',
+    site_url: 'https://andreabianchi.vercel.app',
+    feed_url: 'https://andreabianchi.vercel.app/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
