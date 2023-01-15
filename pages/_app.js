f8f83d81f1bb58c7fc7b3139a79d0059118b193d
Andrea Bianchi <bianan96@gmail.com>
2023-01-15 22:56:15 +0100

    Add Navbar to the website [wip]

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,29 @@
 import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
+import { Navbar } from '../components/Navbar'
 
-import '../styles/main.css'
+import '../styles/main.scss'
 
 export default function Nextra({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <link
-          rel="alternate"
-          type="application/rss+xml"
-          title="RSS"
-          href="/feed.xml"
-        />
-        <link
-          rel="preload"
-          href="/fonts/Inter-roman.latin.var.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+    return (
+        <>
+            <Head>
+                <link
+                    rel="alternate"
+                    type="application/rss+xml"
+                    title="RSS"
+                    href="/feed.xml"
+                />
+                <link
+                    rel="preload"
+                    href="/fonts/Inter-roman.latin.var.woff2"
+                    as="font"
+                    type="font/woff2"
+                    crossOrigin="anonymous"
+                />
+            </Head>
+            <Navbar />
+            <Component {...pageProps} />
+        </>
+    )
 }
