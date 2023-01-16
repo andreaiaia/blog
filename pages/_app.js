c549195e411c0a7ffe82051c9c4b882390eaf525
Andrea Bianchi <bianan96@gmail.com>
2023-01-16 17:07:31 +0100

    Add global state to integrate with navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,39 @@
-import 'nextra-theme-blog/style.css'
-import Head from 'next/head'
-import { Navbar } from '../components/Navbar'
+import { useState } from 'react';
+import Head from 'next/head';
+import { Navbar } from '../components/Navbar';
 
-import '../styles/main.scss'
+import 'nextra-theme-blog/style.css';
+import '../styles/main.scss';
+import '../styles/layout.scss';
 
-export default function Nextra({ Component, pageProps }) {
-    return (
-        <>
-            <Head>
-                <link
-                    rel="alternate"
-                    type="application/rss+xml"
-                    title="RSS"
-                    href="/feed.xml"
-                />
-                <link
-                    rel="preload"
-                    href="/fonts/Inter-roman.latin.var.woff2"
-                    as="font"
-                    type="font/woff2"
-                    crossOrigin="anonymous"
-                />
-            </Head>
-            <Navbar />
-            <Component {...pageProps} />
-        </>
-    )
+import { NavbarContext } from '../Hooks/Context/GlobalContext';
+
+export default function App({ Component, pageProps }) {
+  const [expanded, setExpanded] = useState(true);
+
+  return (
+    <>
+      <Head>
+        <link
+          rel="alternate"
+          type="application/rss+xml"
+          title="RSS"
+          href="/feed.xml"
+        />
+        <link
+          rel="preload"
+          href="/fonts/Inter-roman.latin.var.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <NavbarContext.Provider value={{ expanded, setExpanded }}>
+        <Navbar />
+        <main className={expanded ? '' : 'collapsed'}>
+          <Component {...pageProps} />
+        </main>
+      </NavbarContext.Provider>
+    </>
+  );
 }
