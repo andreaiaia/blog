aa71c5beef39c30bde6e7e92f1ec27b5d41fd17a
Andrea Bianchi <bianan96@gmail.com>
2023-01-16 12:34:49 +0100

    Add animation for opening and closing

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -1,67 +1,99 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+import Link from 'next/link';
 
 import {
   Archive,
-  Bookmark,
   Camera,
   GitHub,
   Home,
   Instagram,
   Linkedin,
   Menu,
-  PenTool,
+  Moon,
+  Sun,
   XCircle,
-} from 'react-feather'
+} from 'react-feather';
 
-import styles from './Navbar.module.scss'
+import styles from './Navbar.module.scss';
 
 export const Navbar = () => {
+  const [expanded, setExpanded] = useState(true);
+  const YEAR = new Date().getFullYear();
+
+  const handleMenu = () => {
+    setExpanded(!expanded);
+  };
+
   return (
-    <header className={styles.header}>
-      <span className={styles.options}>
-        <XCircle />
-      </span>
-      <nav className={styles.nav}>
-        <ul>
-          <li>
-            <a href="./index">
-              <Home className={styles.icon} />
-              Home
+    <>
+      <div className={styles.options}>
+        {expanded ? (
+          <XCircle onClick={handleMenu} />
+        ) : (
+          <Menu onClick={handleMenu} />
+        )}
+        <Moon />
+      </div>
+      <header
+        className={
+          expanded ? `${styles.header}` : `${styles.header} ${styles.collapsed}`
+        }
+      >
+        <nav className={styles.nav}>
+          <ul>
+            <li className={styles.active}>
+              <Link href="./index">
+                <>
+                  <Home className={styles.icon} />
+                  Home
+                </>
+              </Link>
+            </li>
+            <li>
+              <Link href="./Posts">
+                <>
+                  <Archive className={styles.icon} />
+                  Posts
+                </>
+              </Link>
+            </li>
+            <li>
+              <Link href="./Photos">
+                <>
+                  <Camera className={styles.icon} />
+                  Photos
+                </>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <small className={styles.socials}>
+          <ul className={styles.socialsLinks}>
+            <li>
+              <Link href="https://instagram.com/andreaiaia">
+                <Instagram className={styles.icon} />
+              </Link>
+            </li>
+            <li>
+              <Link href="https://github.com/andreaiaia">
+                <GitHub className={styles.icon} />
+              </Link>
+            </li>
+            <li>
+              <Link href="https://linkedin.com/in/andreaiaia/">
+                <Linkedin className={styles.icon} />
+              </Link>
+            </li>
+          </ul>
+          <div className={styles.credits}>
+            <time>{YEAR}</time> © Andrea Bianchi.
+            <a className={styles.rss} href="/feed.xml">
+              RSS
             </a>
-          </li>
-          <li>
-            <a href="./Posts">
-              <Bookmark className={styles.icon} />
-              Posts
-            </a>
-          </li>
-          <li>
-            <a href="./Photos">
-              <Camera className={styles.icon} />
-              Photos
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <small className={styles.socials}>
-        <ul className={styles.socialsLinks}>
-          <li>
-            <a href="https://instagram.com/andreaiaia">
-              <Instagram className={styles.icon} />
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/andreaiaia">
-              <GitHub className={styles.icon} />
-            </a>
-          </li>
-          <li>
-            <a href="https://linkedin.com/in/andreaiaia/">
-              <Linkedin className={styles.icon} />
-            </a>
-          </li>
-        </ul>
-      </small>
-    </header>
-  )
-}
+          </div>
+        </small>
+      </header>
+    </>
+  );
+};
