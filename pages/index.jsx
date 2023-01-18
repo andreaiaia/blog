908950b62f2b8e37617e9bcf92041d4e3f601525
Andrea Bianchi <bianan96@gmail.com>
2023-01-18 18:50:41 +0100

    Make profile pic random

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
+
+import { FileText } from 'react-feather';
 
 import styles from '../styles/Home.module.scss';
 
 const index = () => {
+  const [profilePic, setProfilePic] = useState('/images/bio/me-1.jpg');
+
+  useEffect(() => {
+    const random = Math.floor(Math.random() * 4) + 1;
+    setProfilePic(`/images/bio/me-${random}.jpg`);
+  }, []);
+
   return (
     <article className={styles.container}>
-      <section>
-        <Image
-          src="/images/bio/me-1.jpg"
-          alt="A boring picture of me, Andrea"
-          width={200}
-          height={200}
-          priority
-          className={styles.proPic}
-        />
-        <h1 className={styles.title}>Hi, I'm Andrea Bianchi</h1>
-        <p>This is my home</p>
+      <section className={styles.sectionBio}>
+        <div className={`${styles.card} ${styles.img}`}>
+          <Image
+            src={profilePic}
+            alt="A boring picture of me, Andrea"
+            width={400}
+            height={400}
+            priority
+            className={styles.proPic}
+          />
+        </div>
+        <div className={styles.card}>
+          <div>
+            <p className={styles.greetings}>Hi, I'm</p>
+            <h1 className={styles.title}>Andrea Bianchi</h1>
+            <button className={styles.download}>
+              <FileText /> Download CV
+            </button>
+          </div>
+        </div>
       </section>
     </article>
   );
