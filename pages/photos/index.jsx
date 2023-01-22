d97e9250a59645a51a4ce8aaca2af33edea4904e
Andrea Bianchi <bianan96@gmail.com>
2023-01-22 12:33:53 +0100

    Dynamically generate the gallery categories

--- pages/photos/index.jsx
+++ pages/photos/index.jsx
@@ -2,19 +2,26 @@ import React from 'react';
 
 import Thumbnail from '../../components/Thumbnail';
 
+import photoData from './photos-index.json';
+
 import styles from './Photos.module.scss';
 
 const index = () => {
+  const data = photoData.data;
+
   return (
     <article className={styles.container}>
       <h1>Photo Gallery</h1>
-      <section className={styles.group}>
-        <Thumbnail
-          src="/images/photos/Astro/thumbnail.webp"
-          alt="View my pictures of the deep sky"
-          to="/photos/Astrophotography"
-          description="astrophotography"
-        />
+      <section className={styles.thumbnails}>
+        {data.map((item, index) => (
+          <Thumbnail
+            key={index}
+            src={item.path + '/' + item.thumbnail}
+            alt={item.description}
+            to={'/photos/' + item.path.slice(15)}
+            description={item.category}
+          />
+        ))}
       </section>
     </article>
   );
