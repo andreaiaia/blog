02e76aca143e64c11e8104228a2a82bab6d2b4bd
Andrea Bianchi <bianan96@gmail.com>
2023-01-28 22:51:14 +0100

    Start structuring and formatting post page

--- pages/posts/[id].jsx
+++ pages/posts/[id].jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
+import styles from './Blog.module.scss';
+
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
   return {
@@ -19,12 +21,27 @@ export async function getStaticPaths() {
 }
 
 const Post = ({ postData }) => {
+  const { data, content } = postData;
+  const formattedDate = new Date(data.date).toDateString();
+  const tags = data.tag.split(', ');
+
   return (
-    <div>
-      <h1>{postData.title}</h1>
-      <div>{postData.date}</div>
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-    </div>
+    <article className={styles.container}>
+      <h1>{data.title}</h1>
+      <div className={styles.meta}>
+        <div>{formattedDate}</div>
+        <div className={styles.tags}>
+          {tags.map((tag, index) => {
+            return (
+              <p key={index} className={styles.tag}>
+                {tag}
+              </p>
+            );
+          })}
+        </div>
+      </div>
+      <div dangerouslySetInnerHTML={{ __html: content }} />
+    </article>
   );
 };
 
