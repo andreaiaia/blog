5501db32f757b3e37226f655835de56abb8644cc
Andrea Bianchi <bianan96@gmail.com>
2023-01-28 21:38:19 +0100

    Fix build error when attempting to generate the rss feed

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -1,38 +1,40 @@
-const { promises: fs } = require('fs')
-const path = require('path')
-const RSS = require('rss')
-const matter = require('gray-matter')
+const { promises: fs } = require('fs');
+const path = require('path');
+const RSS = require('rss');
+const matter = require('gray-matter');
 
 async function generate() {
   const feed = new RSS({
     title: 'Andrea Bianchi',
     site_url: 'https://andreabianchi.vercel.app',
-    feed_url: 'https://andreabianchi.vercel.app/feed.xml'
-  })
+    feed_url: 'https://andreabianchi.vercel.app/feed.xml',
+  });
 
-  const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
+  const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'));
+
+  const regex = new RegExp(/\.md$/, 'i');
 
   await Promise.all(
-    posts.map(async (name) => {
-      if (name.startsWith('index.')) return
+    posts
+      .filter((fileName) => regex.test(fileName))
+      .map(async (name) => {
+        const content = await fs.readFile(
+          path.join(__dirname, '..', 'pages', 'posts', name)
+        );
+        const frontmatter = matter(content);
 
-      const content = await fs.readFile(
-        path.join(__dirname, '..', 'pages', 'posts', name)
-      )
-      const frontmatter = matter(content)
-
-      feed.item({
-        title: frontmatter.data.title,
-        url: '/posts/' + name.replace(/\.mdx?/, ''),
-        date: frontmatter.data.date,
-        description: frontmatter.data.description,
-        categories: frontmatter.data.tag.split(', '),
-        author: frontmatter.data.author
+        feed.item({
+          title: frontmatter.data.title,
+          url: '/posts/' + name.replace(/\.mdx?/, ''),
+          date: frontmatter.data.date,
+          description: frontmatter.data.description,
+          categories: frontmatter.data.tag.split(', '),
+          author: frontmatter.data.author,
+        });
       })
-    })
-  )
+  );
 
-  await fs.writeFile('./public/feed.xml', feed.xml({ indent: true }))
+  await fs.writeFile('./public/feed.xml', feed.xml({ indent: true }));
 }
 
-generate()
+generate();
