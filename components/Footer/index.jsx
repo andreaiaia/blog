b34a12178a77dd2395b2b1b271aa91331dbfbec1
Andrea Bianchi <bianan96@gmail.com>
2023-02-22 16:25:19 +0100

    Write base css for footer layout

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,55 +1,36 @@
 import React from 'react';
 
-import {
-  Archive,
-  Camera,
-  GitHub,
-  User,
-  Instagram,
-  Linkedin,
-  Menu,
-  XCircle,
-} from 'react-feather';
+import { GitHub, Instagram, Linkedin } from 'react-feather';
+
+import styles from './Footer.module.scss';
 
 const Footer = () => {
   const YEAR = new Date().getFullYear();
 
   return (
-    <footer>
-      {/* <div className={styles.options}>
-        <button onClick={handleMenu}>
-          <Menu alt="Open menu button" />
-        </button>
-        <ThemeSwitcher />
-      </div>
-      <button className={styles.closeMenu} onClick={handleMenu}>
-        <XCircle alt="Close menu button" />
-      </button> */}
-
-      <small className={styles.socials}>
-        <ul className={styles.socialsLinks}>
-          <li>
-            <a href="https://instagram.com/andreaiaia" target="_blank">
-              <Instagram className={styles.icon} alt="Instagram logo" />
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/andreaiaia" target="_blank">
-              <GitHub className={styles.icon} alt="Github logo" />
-            </a>
-          </li>
-          <li>
-            <a href="https://linkedin.com/in/andreaiaia/" target="_blank">
-              <Linkedin className={styles.icon} alt="Linkedin logo" />
-            </a>
-          </li>
-        </ul>
-        <div className={styles.credits}>
-          <time>{YEAR}</time> © Andrea Bianchi.{' '}
-          <a className={styles.rss} href="/feed.xml">
-            RSS
+    <footer className={styles.footer}>
+      <ul className={styles.socials}>
+        <li>
+          <a href="https://instagram.com/andreaiaia" target="_blank">
+            <Instagram className={styles.icon} alt="Instagram logo" />
           </a>
-        </div>
+        </li>
+        <li>
+          <a href="https://github.com/andreaiaia" target="_blank">
+            <GitHub className={styles.icon} alt="Github logo" />
+          </a>
+        </li>
+        <li>
+          <a href="https://linkedin.com/in/andreaiaia/" target="_blank">
+            <Linkedin className={styles.icon} alt="Linkedin logo" />
+          </a>
+        </li>
+      </ul>
+      <small className={styles.credits}>
+        <time>{YEAR}</time> © Andrea Bianchi.
+      </small>
+      <small className={styles.rss}>
+        <a href="/feed.xml">RSS</a>
       </small>
     </footer>
   );
