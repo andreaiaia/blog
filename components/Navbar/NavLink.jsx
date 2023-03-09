3517d4dda78b0c2616622d6dd7c19c80c6de39e4
Andrea Bianchi <bianan96@gmail.com>
2023-03-09 14:05:11 +0100

    Clean up component, use Link component following new specs

--- components/Navbar/NavLink.jsx
+++ components/Navbar/NavLink.jsx
@@ -1,26 +1,19 @@
-import React, { Children } from 'react';
+import React from 'react';
 
 import { useRouter } from 'next/router';
-import cx from 'classnames';
 import Link from 'next/link';
 
 import styles from './NavLink.module.scss';
 
-const NavLink = ({ children, ...props }) => {
+const NavLink = (props) => {
   const { asPath } = useRouter();
-  const child = Children.only(children);
-  const childClassName = child.props.className || '';
 
   const isActive = asPath === props.href || asPath === props.as;
 
-  const className = cx(childClassName, { [styles.active]: isActive });
-
   return (
     <li>
-      <Link {...props} legacyBehavior>
-        {React.cloneElement(child, {
-          className: className || null,
-        })}
+      <Link {...props} className={isActive ? styles.active : ''}>
+        {props.children}
       </Link>
     </li>
   );
