1c98a7de142df014dedaea3c71e115561e1b30bf
Andrea Bianchi <bianan96@gmail.com>
2023-03-09 14:05:39 +0100

    Change Link to adapt to new behaviour

--- components/Navbar/NavItems.jsx
+++ components/Navbar/NavItems.jsx
@@ -4,20 +4,24 @@ import NavLink from './NavLink';
 
 import styles from './NavItems.module.scss';
 
-const NavItems = () => {
+const NavItems = ({ menuHandler }) => {
   return (
     <ul className={styles.navLinks}>
-      <NavLink href="/">
-        <a>About</a>
+      <NavLink href="/" onClick={menuHandler}>
+        About
       </NavLink>
-      <NavLink href="/posts">
-        <a>Blog</a>
+      <NavLink href="/posts" onClick={menuHandler}>
+        Blog
       </NavLink>
-      <NavLink href="/photos">
-        <a>Photos</a>
+      <NavLink href="/photos" onClick={menuHandler}>
+        Photos
       </NavLink>
     </ul>
   );
+};
+
+NavItems.defaultProps = {
+  menuHandler: () => null,
 };
 
 export default NavItems;
