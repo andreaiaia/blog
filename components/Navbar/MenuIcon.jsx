763e47d63a3007d1c5748241a29cabc7c0dbf120
Andrea Bianchi <bianan96@gmail.com>
2023-03-09 14:04:29 +0100

    Fix typo

--- components/Navbar/MenuIcon.jsx
+++ components/Navbar/MenuIcon.jsx
@@ -13,9 +13,9 @@ const MenuIcon = ({ isOpen, handler }) => {
         height="24"
         viewBox="0 0 24 24"
         stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       >
         <motion.path
           animate={isOpen ? 'open' : 'closed'}
