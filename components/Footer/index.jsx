2a95913b3cc257e0985f959a8519b6d383369d04
Andrea Bianchi <bianan96@gmail.com>
2023-03-23 15:39:00 +0100

    Fix footer spacing on larger screens

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -28,43 +28,45 @@ const Footer = () => {
 
   return (
     <footer className={styles.footer}>
-      <ul className={styles.links}>
-        <li>
-          <a href="/feed.xml">
-            <Rss className={styles.icon} alt="RSS feed" />
+      <div className={styles.content}>
+        <ul className={styles.links}>
+          <li>
+            <a href="/feed.xml">
+              <Rss className={styles.icon} alt="RSS feed" />
+            </a>
+          </li>
+          <li>
+            <a href="https://instagram.com/andreaiaia" target="_blank">
+              <Instagram className={styles.icon} alt="Instagram logo" />
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/andreaiaia" target="_blank">
+              <GitHub className={styles.icon} alt="Github logo" />
+            </a>
+          </li>
+          <li>
+            <a href="https://linkedin.com/in/andreaiaia/" target="_blank">
+              <Linkedin className={styles.icon} alt="Linkedin logo" />
+            </a>
+          </li>
+        </ul>
+        <small className={styles.credits}>
+          <time>{YEAR}</time> © Andrea Bianchi.
+        </small>
+        <div className={styles.themeSwitcher}>
+          <ThemeSwitcher />
+        </div>
+        <div className={styles.notAiBadge}>
+          <a href="https://notbyai.fyi/">
+            <Image
+              src={notByAiBadge}
+              alt="Badge certifying this website and its contents are human made - no AI involved"
+              height={84}
+              width={120}
+            />
           </a>
-        </li>
-        <li>
-          <a href="https://instagram.com/andreaiaia" target="_blank">
-            <Instagram className={styles.icon} alt="Instagram logo" />
-          </a>
-        </li>
-        <li>
-          <a href="https://github.com/andreaiaia" target="_blank">
-            <GitHub className={styles.icon} alt="Github logo" />
-          </a>
-        </li>
-        <li>
-          <a href="https://linkedin.com/in/andreaiaia/" target="_blank">
-            <Linkedin className={styles.icon} alt="Linkedin logo" />
-          </a>
-        </li>
-      </ul>
-      <small className={styles.credits}>
-        <time>{YEAR}</time> © Andrea Bianchi.
-      </small>
-      <div className={styles.themeSwitcher}>
-        <ThemeSwitcher />
-      </div>
-      <div className={styles.notAiBadge}>
-        <a href="https://notbyai.fyi/">
-          <Image
-            src={notByAiBadge}
-            alt="Badge certifying this website and its contents are human made - no AI involved"
-            height={84}
-            width={120}
-          />
-        </a>
+        </div>
       </div>
     </footer>
   );
