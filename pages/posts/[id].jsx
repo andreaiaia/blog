ebba4476bd12ed5f26769ac2cc3b078bbf76c9a4
Andrea Bianchi <bianan96@gmail.com>
2023-03-24 01:09:33 +0100

    Nicely display the dateand reading time

--- pages/posts/[id].jsx
+++ pages/posts/[id].jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import readingTime from 'reading-time';
+
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
 import styles from './Blog.module.scss';
@@ -21,28 +23,42 @@ export async function getStaticPaths() {
   };
 }
 
+const formatDate = (date) => {
+  const options = {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  };
+
+  return new Intl.DateTimeFormat('en-US', options).format(new Date(date));
+};
+
 const Post = ({ postData }) => {
   const { data, content } = postData;
-  const formattedDate = new Date(data.date).toDateString();
+  const formattedDate = formatDate(data.date);
   const tags = data.tag.split(', ');
+  const stats = readingTime(content);
+  console.log(stats);
 
   return (
     <main className={styles.container}>
       <Breadcrumbs />
+      <div className={styles.meta}>
+        <p>{formattedDate}</p>
+        <p className={styles.readingtime}>{stats.text}</p>
+        <p>{stats.words} words</p>
+      </div>
       <div className={styles.title}>
         <h1>{data.title}</h1>
       </div>
-      <div className={styles.meta}>
-        <div>{formattedDate}</div>
-        <div className={styles.tags}>
-          {tags.map((tag, index) => {
-            return (
-              <p key={index} className={styles.tag}>
-                {tag}
-              </p>
-            );
-          })}
-        </div>
+      <div className={styles.tags}>
+        {tags.map((tag, index) => {
+          return (
+            <p key={index} className={styles.tag}>
+              {tag}
+            </p>
+          );
+        })}
       </div>
       <article
         className={styles.postContent}
