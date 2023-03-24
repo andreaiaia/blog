092f038249d943b24645c8dc1f94130730956098
Andrea Bianchi <bianan96@gmail.com>
2023-03-24 12:00:01 +0100

    Add global meta tag

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,7 @@ export default function Document() {
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
+        <meta property="og:type" content="website" />
         {/* <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@yourname" /> */}
       </Head>
