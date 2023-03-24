69a17e1766851ec757012851aaabb48309ec8f4e
Andrea Bianchi <bianan96@gmail.com>
2023-03-24 11:02:04 +0100

    Add head metadata to blog posts

--- pages/posts/[id].jsx
+++ pages/posts/[id].jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Head from 'next/head';
 
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
-import styles from './Blog.module.scss';
-
 import Breadcrumbs from '../../components/Breadcrumbs';
 import PostMetadata from '../../components/PostMetadata';
+
+import styles from './Blog.module.scss';
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
@@ -29,30 +30,39 @@ const Post = ({ postData }) => {
   const tags = data.tag.split(', ');
 
   return (
-    <main className={styles.container}>
-      <Breadcrumbs />
-      <PostMetadata
-        cname={styles.metadata}
-        date={formattedDate}
-        stats={stats}
-      />
-      <div className={styles.title}>
-        <h1>{data.title}</h1>
-      </div>
-      <div className={styles.tags}>
-        {tags.map((tag, index) => {
-          return (
-            <p key={index} className={styles.tag}>
-              {tag}
-            </p>
-          );
-        })}
-      </div>
-      <article
-        className={styles.postContent}
-        dangerouslySetInnerHTML={{ __html: content }}
-      />
-    </main>
+    <>
+      <Head>
+        <title>{data.title} - Andrea</title>
+        <meta property="og:title" content="homepage" key="title" />
+        <meta name="description" content={data.description} />
+        <meta name="keywords" content={data.tag} />
+        <meta name="author" content={data.author} />
+      </Head>
+      <main className={styles.container}>
+        <Breadcrumbs />
+        <PostMetadata
+          cname={styles.metadata}
+          date={formattedDate}
+          stats={stats}
+        />
+        <div className={styles.title}>
+          <h1>{data.title}</h1>
+        </div>
+        <div className={styles.tags}>
+          {tags.map((tag, index) => {
+            return (
+              <p key={index} className={styles.tag}>
+                {tag}
+              </p>
+            );
+          })}
+        </div>
+        <article
+          className={styles.postContent}
+          dangerouslySetInnerHTML={{ __html: content }}
+        />
+      </main>
+    </>
   );
 };
 
