7793b5e4f03cbeb8c4369969b110ff1ee2d6450e
Andrea Bianchi <bianan96@gmail.com>
2023-04-02 20:02:14 +0200

    Add animation to hide and show header

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Navbar from '../Navbar';
 import ThemeSwitcher from '../ThemeSwitcher';
@@ -8,16 +8,42 @@ import styles from './Header.module.scss';
 import Link from 'next/link';
 
 const Header = ({ pages }) => {
+  const [mounted, setMounted] = useState(false);
+  const [prevScroll, setPrevScroll] = useState(0);
+  const [headerStyle, setHeaderStyle] = useState(styles.header);
+
+  const handleScroll = () => {
+    const currentScroll = window.scrollY;
+
+    if (currentScroll > prevScroll) {
+      setHeaderStyle(`${styles.header} ${styles.hidden}`);
+    } else {
+      setHeaderStyle(styles.header);
+    }
+
+    setPrevScroll(currentScroll);
+  };
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  window.addEventListener('scroll', handleScroll);
+
   return (
-    <header className={styles.header}>
-      <div className={styles.logo}>
-        <Link href="/">
-          <Logo />
-        </Link>
-      </div>
-      <Navbar pages={pages} />
-      <div className={styles.themeSwitcher}>
-        <ThemeSwitcher />
+    <header className={headerStyle}>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <Link href="/">
+            <Logo />
+          </Link>
+        </div>
+        <Navbar pages={pages} />
+        <div className={styles.themeSwitcher}>
+          <ThemeSwitcher />
+        </div>
       </div>
     </header>
   );
