203b3b85eb39ac4077e3f01db6b054e9e6743853
Andrea Bianchi <bianan96@gmail.com>
2023-04-18 17:17:19 +0200

    Update components to either act as links or simple divs

--- components/Thumbnails/Polaroid.jsx
+++ components/Thumbnails/Polaroid.jsx
@@ -1,23 +1,34 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 import styles from './Polaroid.module.scss';
 
-const Polaroid = ({ src, alt, cname }) => {
-  return (
-    <div className={`${styles.container} ${cname || ''}`}>
-      <div className={styles.img}>
-        <Image
-          src={src}
-          alt={alt || ''}
-          fill
-          sizes="(max-width: 768px) 300px,
+const PolaroidImage = ({ src, alt }) => (
+  <>
+    <div className={styles.img}>
+      <Image
+        src={src}
+        alt={alt || ''}
+        fill
+        sizes="(max-width: 768px) 300px,
                   500px"
-        />
-      </div>
-      <div className={styles.title}>
-        <p>{alt}</p>
-      </div>
+      />
+    </div>
+    <div className={styles.title}>
+      <p>{alt}</p>
+    </div>
+  </>
+);
+
+const Polaroid = ({ src, alt, cname, to }) => {
+  return to ? (
+    <Link className={`${styles.container} ${cname || ''}`} href={to || '#'}>
+      <PolaroidImage src={src} alt={alt} />
+    </Link>
+  ) : (
+    <div className={`${styles.container} ${cname || ''}`}>
+      <PolaroidImage src={src} alt={alt} />
     </div>
   );
 };
