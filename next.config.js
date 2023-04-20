68861941a455995dbb78a4ce9258fb839d73bd71
Andrea Bianchi <bianan96@gmail.com>
2023-04-20 12:45:03 +0200

    update next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
