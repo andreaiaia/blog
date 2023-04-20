953387f6a27a3dbd2c7f7399e0fe4276cea8aa48
Andrea Bianchi <bianan96@gmail.com>
2023-04-20 16:50:24 +0200

    remove unsplash from next.config allowed hosts

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
