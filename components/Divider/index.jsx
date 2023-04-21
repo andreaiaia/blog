6585cb8a66697c5f693cf99fa0c8f8f4430cf262
Andrea Bianchi <bianan96@gmail.com>
2023-04-21 10:37:22 +0200

    Add options to divider component

--- components/Divider/index.jsx
+++ components/Divider/index.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 import styles from './Divider.module.scss';
 
-const Divider = ({ size }) => {
-  return <hr className={styles.separator} />;
+const Divider = ({ size, cname }) => {
+  return <hr className={`${styles.separator} ${styles[size]} ${cname}`} />;
 };
 
 export default Divider;
