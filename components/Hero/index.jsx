e4d2d4093d70d01c564ad7d59a499e79553e2839
Andrea Bianchi <bianan96@gmail.com>
2023-04-22 15:43:01 +0200

    Update Hero component

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import styles from './Hero.module.scss';
 
-const Hero = () => {
+import css from './Hero.module.scss';
+
+const Hero = ({ children, cname, background }) => {
   return (
-    <section className={styles.container}>
-      {/* At the moment I like the hero like this, empty */}
+    <section className={`${css.hero} ${css[background]}`}>
+      <div className={`${css.content} ${cname}`}>{children}</div>
     </section>
   );
 };
