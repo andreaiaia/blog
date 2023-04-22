f2355e749c9c9c2b6403b98906aa32c5c7f42e4a
Andrea Bianchi <bianan96@gmail.com>
2023-04-22 18:15:09 +0200

    replace styles with css

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,7 +8,7 @@ import { Polaroid } from '../components/Thumbnails';
 import Hero from '../components/Hero';
 
 import Matera from '../public/images/photos/matera/thumbnail.webp';
-import styles from '../styles/Home.module.scss';
+import css from '../styles/Home.module.scss';
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -25,22 +25,22 @@ const Home = ({ allPostsData }) => {
         <meta property="og:title" content="homepage" key="title" />
       </Head>
       <main>
-        <Hero cname={styles.hero} background="transparent">
-          <div className={styles.greetings}>
+        <Hero cname={css.hero} background="transparent">
+          <div className={css.greetings}>
             <h1>Hello, my name is Andrea</h1>
             <p>
               I&apos;m a Front-End Developer from{' '}
-              <span className={styles.accent}>Matera, Italy</span>
+              <span className={css.accent}>Matera, Italy</span>
             </p>
           </div>
-          <Polaroid cname={styles.pic} src={Matera} alt="My home, Matera" />
+          <Polaroid cname={css.pic} src={Matera} alt="My home, Matera" />
         </Hero>
-        <section className={styles.recentPosts}>
-          <div className={styles.title}>
+        <section className={css.recentPosts}>
+          <div className={css.title}>
             <h2>Latest posts</h2>
           </div>
-          <div className={styles.postsContainer}>
-            <ul className={styles.posts}>
+          <div className={css.postsContainer}>
+            <ul className={css.posts}>
               {allPostsData
                 .slice(0, 3)
                 .map(
