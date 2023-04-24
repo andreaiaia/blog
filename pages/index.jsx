b20d80bcd3d8222539093ecd7bb006c0f6aa6489
Andrea Bianchi <bianan96@gmail.com>
2023-04-24 17:36:20 +0200

    Add CTAs to homepage

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Head from 'next/head';
+import Link from 'next/link';
 
 import { getSortedPostsData } from '../lib/posts';
 
 import { PostCard } from '../components/Thumbnails';
 import { Polaroid } from '../components/Thumbnails';
 import Hero from '../components/Hero';
+import Button from '../components/Button';
 
 import Matera from '../public/images/photos/matera/thumbnail.webp';
 import css from '../styles/Home.module.scss';
@@ -29,9 +31,18 @@ const Home = ({ allPostsData }) => {
           <div className={css.greetings}>
             <h1>Hello, my name is Andrea</h1>
             <p>
-              I&apos;m a Front-End Developer from{' '}
-              <span className={css.accent}>Matera, Italy</span>
+              I&apos;m a <span className={css.accent}>Front-End Developer</span>{' '}
+              from Matera, Italy and in my free time I love taking{' '}
+              <span className={css.accent}>photos</span>.
             </p>
+            <div className={css.CTAs}>
+              <Button className={css.homeCta}>
+                <Link href="/posts">Read my Blog</Link>
+              </Button>
+              <Button className={css.homeCta} secondary>
+                <Link href="/photos">See my Photos</Link>
+              </Button>
+            </div>
           </div>
           <Polaroid cname={css.pic} src={Matera} alt="My home, Matera" />
         </Hero>
