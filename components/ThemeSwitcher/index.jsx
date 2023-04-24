23c7a07179825a9ae982877ad3471f2b174db0c0
Andrea Bianchi <bianan96@gmail.com>
2023-04-24 17:35:32 +0200

    Add prop to themeswitcher component

--- components/ThemeSwitcher/index.jsx
+++ components/ThemeSwitcher/index.jsx
@@ -5,7 +5,7 @@ import { Moon, Sun } from 'react-feather';
 
 import styles from './ThemeSwitcher.module.scss';
 
-const ThemeSwitcher = () => {
+const ThemeSwitcher = ({ cname }) => {
   const [mounted, setMounted] = useState(false);
   const { setTheme } = useTheme();
 
@@ -17,10 +17,16 @@ const ThemeSwitcher = () => {
 
   return (
     <>
-      <button onClick={() => setTheme('dark')} className={styles.Moon}>
+      <button
+        onClick={() => setTheme('dark')}
+        className={`${styles.Moon} ${cname}`}
+      >
         <Moon />
       </button>
-      <button onClick={() => setTheme('light')} className={styles.Sun}>
+      <button
+        onClick={() => setTheme('light')}
+        className={`${styles.Sun} ${cname}`}
+      >
         <Sun />
       </button>
     </>
