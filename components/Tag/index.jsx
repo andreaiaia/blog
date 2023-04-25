b6f18a3f832e945a7cfa4171f6dd536afb789d10
Andrea Bianchi <bianan96@gmail.com>
2023-04-25 17:05:23 +0200

    Refactor tag component

--- components/Tag/index.jsx
+++ components/Tag/index.jsx
@@ -3,12 +3,19 @@ import Link from 'next/link';
 
 import styles from './Tag.module.scss';
 
-const Tag = ({ tag, to }) => {
-  return (
-    <Link href={to || '#'} className={styles.tag}>
-      {tag}
-    </Link>
-  );
+const Tag = ({ tag, to, toAnchor }) => {
+  if (toAnchor)
+    return (
+      <a href={to} className={styles.tag}>
+        {tag}
+      </a>
+    );
+  else
+    return (
+      <Link href={to || '#'} className={styles.tag}>
+        {tag}
+      </Link>
+    );
 };
 
 export default Tag;
