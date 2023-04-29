756dc1b16d100c63ebe0c161fe73d85563d5add1
Andrea Bianchi <bianan96@gmail.com>
2023-04-29 15:55:42 +0200

    Update 404 page style

--- pages/404.jsx
+++ pages/404.jsx
@@ -3,23 +3,25 @@ import Image from 'next/image';
 
 import Hero from '../components/Hero';
 
-import css from '../styles/Home.module.scss';
+import css from '../styles/PageNotFound.module.scss';
 import pic from '../public/images/photos/blackandwhite/Matera-2021-5.webp';
 
 const pageNotFound = () => {
   return (
     <main>
-      <Hero background="tertiary" cname={css.pageNotFound}>
-        <h1>
-          404 <br /> page not found
-        </h1>
+      <Hero cname={css.pageNotFound}>
+        <div className={css.title}>
+          <p>404</p>
+          <h1>Page not found</h1>
+        </div>
         <div className={css.pageNotFoundPic}>
           <Image
             src={pic}
             alt="The Sassi of Matera in a double exposure with the clouds"
             placeholder="blur"
             fill
-            sizes=""
+            sizes="(max-width: 768px) 300px,
+                  500px"
           />
         </div>
       </Hero>
