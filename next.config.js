428915cd691735675d5c5c478f86639d9c850bcb
Andrea Bianchi <bianan96@gmail.com>
2023-05-02 19:54:54 +0200

    Add notion api source to allowed remotePatterns

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 's3.us-west-2.amazonaws.com',
+        port: '',
+        pathname: '/secure.notion-static.com/**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
