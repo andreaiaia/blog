7cf727f9bd1a2885c901bd5239950feda6944f92
Andrea Bianchi <bianan96@gmail.com>
2023-05-18 10:47:05 +0200

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 's3.us-west-2.amazonaws.com',
-        port: '',
-        pathname: '/secure.notion-static.com/**',
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
