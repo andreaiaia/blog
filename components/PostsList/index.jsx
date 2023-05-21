2d021bb22363559396c7fa1f2d52f27b50170f0b
Andrea Bianchi <bianan96@gmail.com>
2023-05-21 11:04:52 +0200

    Change export method of the component

--- components/PostsList/index.jsx
+++ components/PostsList/index.jsx
@@ -1,31 +1,4 @@
-import React from 'react';
+import SimpleList from './SimpleList';
+import CardList from './CardList';
 
-import Post from '../Thumbnails/Post';
-
-import css from './PostsList.module.scss';
-
-const PostsList = ({ posts, title }) => {
-  return (
-    <section className={css.container}>
-      {title && <h2>{title}</h2>}
-      <ul className={css.posts}>
-        {posts.map(
-          ({ id, title, description, formattedDate, stats, pic }, index) => (
-            <li key={index}>
-              <Post
-                id={id}
-                title={title}
-                description={description}
-                date={formattedDate}
-                stats={stats}
-                postPic={pic}
-              />
-            </li>
-          )
-        )}
-      </ul>
-    </section>
-  );
-};
-
-export default PostsList;
+export { SimpleList, CardList };
