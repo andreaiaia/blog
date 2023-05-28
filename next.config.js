2ff5933a7e8137f7be165fd0580a90136882e74d
Andrea Bianchi <bianan96@gmail.com>
2023-05-28 22:53:11 +0200

    Configured typescript

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack(config) {
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+  },
 };
 
 module.exports = nextConfig;
