d137e925d8791541f6015d8d794f88cc2552ca53
Andrea Bianchi <bianan96@gmail.com>
2023-05-31 23:39:58 +0200

    Move divider to new components folder and convert to typescript

--- src/components/Divider/index.tsx
+++ src/components/Divider/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { DividerPropsType } from '../types';
 
 import styles from './Divider.module.scss';
 
-const Divider = ({ size, cname }) => {
+const Divider = ({ size, cname }: DividerPropsType) => {
   return <hr className={`${styles.separator} ${styles[size]} ${cname}`} />;
 };
 
