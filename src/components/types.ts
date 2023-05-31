ce8e94053c4a74184f9e866edfd04fcdd930af0d
Andrea Bianchi <bianan96@gmail.com>
2023-05-31 23:40:25 +0200

    Create types for components props

---
+++ src/components/types.ts
@@ -0,0 +1,34 @@
+export type StatsType = {
+  [key: string]: string;
+};
+
+export type PolaroidPropsType = {
+  src: string;
+  alt: string;
+  to: string;
+  cname?: string;
+};
+
+export type GalleryPropsType = Omit<PolaroidPropsType, 'cname'>;
+
+export type ImagePropsType = Omit<GalleryPropsType, 'to'>;
+
+export type PostPropsType = {
+  slug: string;
+  title: string;
+  description: string;
+  date: string;
+  stats: StatsType;
+  postPic: string;
+};
+
+export type PostMetadataPropsType = {
+  date: string;
+  stats: StatsType;
+  cname?: string;
+};
+
+export type DividerPropsType = {
+  size: string;
+  cname?: string;
+};
