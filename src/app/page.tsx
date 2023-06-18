f3ddf9f336370c615b3223d760d79c0be4e439b6
Andrea Bianchi <bianan96@gmail.com>
2023-06-18 22:48:17 +0200

    Use new SEO component in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Head from 'next/head';
+import { Metadata } from 'next';
 import Link from 'next/link';
 
 import { getLatestPostsData } from '../lib/posts';
@@ -18,38 +18,36 @@ export async function getData() {
   return postsData;
 }
 
+export const metadata: Metadata = {
+  title: 'Home - Andrea',
+};
+
 const Home = () => {
   const postsData = getData();
 
   return (
-    <>
-      <Head>
-        <title>Home - Andrea</title>
-        <meta property="og:title" content="homepage" key="title" />
-      </Head>
-      <main>
-        <Hero cname={css.hero} background="transparent">
-          <div className={css.greetings}>
-            <h1>Hello, my name is Andrea</h1>
-            <p>
-              I&apos;m a <span className={css.accent}>Front-End Developer</span>{' '}
-              from Matera, Italy and in my free time I love taking{' '}
-              <span className={css.accent}>photos</span>.
-            </p>
-            <div className={css.CTAs}>
-              <Button cname={css.homeCta}>
-                <Link href="/posts">Read my Blog</Link>
-              </Button>
-              <Button cname={css.homeCta} secondary>
-                <Link href="/photos">See my Photos</Link>
-              </Button>
-            </div>
+    <main>
+      <Hero cname={css.hero} background="transparent">
+        <div className={css.greetings}>
+          <h1>Hello, my name is Andrea</h1>
+          <p>
+            I&apos;m a <span className={css.accent}>Front-End Developer</span>{' '}
+            from Matera, Italy and in my free time I love taking{' '}
+            <span className={css.accent}>photos</span>.
+          </p>
+          <div className={css.CTAs}>
+            <Button cname={css.homeCta}>
+              <Link href="/posts">Read my Blog</Link>
+            </Button>
+            <Button cname={css.homeCta} secondary>
+              <Link href="/photos">See my Photos</Link>
+            </Button>
           </div>
-          <Polaroid cname={css.pic} src={Matera} alt="My home, Matera" />
-        </Hero>
-        <CardList posts={postsData} title="Latest posts" />
-      </main>
-    </>
+        </div>
+        <Polaroid cname={css.pic} src={Matera} alt="My home, Matera" />
+      </Hero>
+      <CardList posts={postsData} title="Latest posts" />
+    </main>
   );
 };
 
