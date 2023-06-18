49ba9cfb428f5d9254c73414a24f2915ec1017a1
Andrea Bianchi <bianan96@gmail.com>
2023-06-18 14:47:10 +0200

    Fix props name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,22 +6,21 @@ import { getLatestPostsData } from '../../lib/posts';
 
 import { Polaroid } from '../components/Thumbnails';
 import Hero from '../../components/Hero';
-import Button from '../../components/Button';
+import Button from '../components/Button';
 import { CardList } from '../../components/PostsList';
 
 import Matera from '/public/images/photos/matera/thumbnail.webp';
 import css from '/styles/Home.module.scss';
 
-export async function getStaticProps() {
+export async function getData() {
   const postsData = await getLatestPostsData();
 
-  return {
-    props: { postsData },
-    revalidate: 10, // In seconds
-  };
+  return postsData;
 }
 
-const Home = ({ postsData }) => {
+const Home = () => {
+  const postsData = getData();
+
   return (
     <>
       <Head>
@@ -38,10 +37,10 @@ const Home = ({ postsData }) => {
               <span className={css.accent}>photos</span>.
             </p>
             <div className={css.CTAs}>
-              <Button className={css.homeCta}>
+              <Button cname={css.homeCta}>
                 <Link href="/posts">Read my Blog</Link>
               </Button>
-              <Button className={css.homeCta} secondary>
+              <Button cname={css.homeCta} secondary>
                 <Link href="/photos">See my Photos</Link>
               </Button>
             </div>
