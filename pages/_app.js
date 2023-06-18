251d07d97ffb577522408cca23cd8fe3c76e7f89
Andrea Bianchi <bianan96@gmail.com>
2023-06-18 14:47:03 +0200

    Update imports

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import Head from 'next/head';
 import { ThemeProvider } from 'next-themes';
 
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Header from '../src/components/Header';
+import Footer from '../src/components/Footer';
 
 import '../styles/normalize.css';
 import '../styles/main.scss';
