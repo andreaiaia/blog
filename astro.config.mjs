05880cee832974a58aacd593ff174fc0c1141e97
Andrea Bianchi <bianan96@gmail.com>
2024-02-10 17:20:26 +0100

    Add react plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import sitemap from '@astrojs/sitemap';
 import svelte from '@astrojs/svelte';
 import vercel from '@astrojs/vercel/serverless';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), svelte()],
+  integrations: [mdx(), sitemap(), svelte(), react()],
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel()
 });
