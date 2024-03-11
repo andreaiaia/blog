168bfdf8248828958f33ad33ef721c72c2878085
Andrea Bianchi <bianan96@gmail.com>
2024-03-11 20:00:31 +0100

    Configure migration to cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import svelte from '@astrojs/svelte';
-import vercel from '@astrojs/vercel/serverless';
+import cloudflare from '@astrojs/cloudflare';
 
 import react from '@astrojs/react';
 
@@ -11,7 +11,11 @@ export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), svelte(), react()],
   output: 'server',
-  adapter: vercel(),
+  adapter: cloudflare({
+    runtime: {
+      mode: 'local',
+    },
+  }),
   vite: {
     server: {
       watch: {
