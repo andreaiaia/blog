7cc393ebf59df73e1eb5555ef8ff916e00098266
Andrea Bianchi <bianan96@gmail.com>
2024-03-20 22:42:12 +0100

    Migrated Tag Component

---
+++ src/components/Tag/Tag.svelte
@@ -0,0 +1,17 @@
+<script>
+  import css from './Tag.module.scss'
+
+  export let tag
+  export let to
+  export let anchor
+</script>
+
+{#if anchor}
+  <a href={to} class={css.tag}>
+    {tag}
+  </a>
+{:else}
+  <a href={to || '#'} class={css.tag}>
+    {tag}
+  </a>
+{/if}
