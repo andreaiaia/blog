1db04e9515aeedc1401ec5e2a2647b0343fd5a64
Andrea Bianchi <bianan96@gmail.com>
2024-03-20 22:42:30 +0100

    Migrated PostMetadata component

---
+++ src/components/PostMetadata/PostMetadata.svelte
@@ -0,0 +1,13 @@
+<script>
+  import css from './PostMetadata.module.scss'
+
+  export let cname
+  export let date
+  export let stats
+</script>
+
+<div class={`${css.meta} ${cname}`}>
+  <p>{date}</p>
+  <p class={css.readingtime}>{stats.text}</p>
+  <p>{stats.words} words</p>
+</div>
