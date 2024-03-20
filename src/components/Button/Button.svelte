9948bc3040e4bff2735d65216e721386d1bf6551
Andrea Bianchi <bianan96@gmail.com>
2024-03-20 22:54:29 +0100

    Migrate Button Component

---
+++ src/components/Button/Button.svelte
@@ -0,0 +1,16 @@
+<script>
+  import css from './Button.module.scss'
+
+  export let cname
+  export let secondary // boolean
+  export let type = 'button' // or 'submit' or 'reset'
+  export let onClick
+</script>
+
+<button
+  {type}
+  {onClick}
+  class={`${css.button} ${cname} ${secondary && css.secondary}`}
+>
+  <slot />
+</button>
