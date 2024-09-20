e6fc15a03e59875b05eb81b46d4302329f83fdfd
Andrea Bianchi <bianan96@gmail.com>
2024-09-20 19:27:58 +0200

    add global const

--- src/consts.ts
+++ src/consts.ts
@@ -2,3 +2,19 @@
 export const SITE_TITLE = 'Andrea';
 export const SITE_DESCRIPTION =
   'Andrea is a Photographer, a Developer, a Traveler, and so much more. He is a creative person who loves to create meaningful things. Welcome to my corner of the internet.';
+
+// Navigation menu items
+export const NAV_PAGES = [
+  {
+    name: 'Photo',
+    href: '/photo',
+  },
+  {
+    name: 'Dev',
+    href: '/dev',
+  },
+  {
+    name: 'Blog',
+    href: '/blog',
+  },
+];
