d135934e09e71596856ba708cb45269f1c1550ea
Andrea Bianchi <bianan96@gmail.com>
2024-09-20 16:30:52 +0200

    Update site description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,4 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Andrea F.';
-export const SITE_DESCRIPTION = 'Questo è il mio blog e di nessun altro!';
+// Site metadata
+export const SITE_TITLE = 'Andrea';
+export const SITE_DESCRIPTION =
+  'Andrea is a Photographer, a Developer, a Traveler, and so much more. He is a creative person who loves to create meaningful things. Welcome to my corner of the internet.';
