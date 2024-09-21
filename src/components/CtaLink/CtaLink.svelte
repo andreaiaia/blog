87fc105fdf9acd828464fd5b3f5a8c1d00dee813
Andrea Bianchi <bianan96@gmail.com>
2024-09-21 17:39:03 +0200

    create cta component

---
+++ src/components/CtaLink/CtaLink.svelte
@@ -0,0 +1,11 @@
+<script>
+  import css from './CtaLink.module.scss'
+
+  export let linkTo = ''
+  export let text = ''
+  export let addStyle = ''
+</script>
+
+<a class={`${css.cta} ${addStyle}`} href={linkTo} data-text={text}>
+  <slot />
+</a>
