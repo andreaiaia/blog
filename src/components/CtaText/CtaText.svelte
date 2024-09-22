698a8456f5d6e18d5af6b57d325c8f654097e2eb
Andrea Bianchi <bianan96@gmail.com>
2024-09-22 14:24:08 +0200

    update cta component

---
+++ src/components/CtaText/CtaText.svelte
@@ -0,0 +1,10 @@
+<script>
+  import css from './CtaText.module.scss'
+
+  export let text = ''
+  export let addStyle = ''
+</script>
+
+<p class={`${css.cta} ${addStyle}`} data-text={text}>
+  <slot />
+</p>
