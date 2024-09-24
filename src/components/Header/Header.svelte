2279745bc4c937f8bbf6ffd44788de9b3498593a
Andrea Bianchi <bianan96@gmail.com>
2024-09-24 11:50:54 +0200

    Redesign header and navbar components

--- src/components/Header/Header.svelte
+++ src/components/Header/Header.svelte
@@ -1,60 +1,16 @@
 <script>
-  import { onMount } from 'svelte'
-  import Logo from '@components/Logo/Logo.svelte'
-  import Navbar from './Navbar.svelte'
+  import Navbar from '../Navbar/Navbar.svelte'
 
   import css from './Header.module.scss'
 
   export let addStyle = ''
-  let mounted = false
-  let prevScroll = 0
-  let headerStyle = css.header
-  let open
-
-  const handleMenu = () => {
-    open = !open
-  }
-
-  const headerStyles = {
-    top: `${css.header}`,
-    hidden: `${css.header} ${css.hidden}`,
-    visible: `${css.header} ${css.background}`
-  }
-
-  const handleScroll = () => {
-    const currentScroll = window.scrollY
-
-    if (currentScroll > prevScroll) headerStyle = headerStyles.hidden
-    else if (currentScroll < 200) {
-      headerStyle = headerStyles.top
-      return
-    } else headerStyle = headerStyles.visible
-
-    prevScroll = currentScroll
-  }
-
-  onMount(() => {
-    mounted = true
-    window.addEventListener('scroll', handleScroll)
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll)
-    }
-  })
 </script>
 
-<header
-  id="top"
-  class={open
-    ? `${addStyle} ${headerStyle} ${css.open}`
-    : `${headerStyle} ${addStyle}`}
->
+<header class={`${css.header} ${addStyle}`}>
   <div class={css.container}>
     <div class={css.logo}>
-      <a href="/">
-        <Logo />
-      </a>
+      <a href="/"> andrea </a>
     </div>
-    <Navbar {open} {handleMenu} />
+    <Navbar />
   </div>
 </header>
---
+++ src/components/Navbar/Navbar.svelte
@@ -0,0 +1,23 @@
+<script>
+  import css from './Navbar.module.scss'
+
+  let menuOpen = false
+
+  function toggleMenu() {
+    menuOpen = !menuOpen
+    console.log(menuOpen)
+  }
+</script>
+
+<nav class={css.navbar}>
+  <ul class={`${css.navLinks} ${menuOpen ? css.active : ''}`}>
+    <li class={css.link}><a href="/about">about</a></li>
+    <li class={css.link}><a href="/photo">photo</a></li>
+    <li class={css.link}><a href="/blog">blog</a></li>
+  </ul>
+  <div class={css.hamburger} on:click={toggleMenu}>
+    <div class={`${css.line} ${menuOpen ? css.change : ''}`}></div>
+    <div class={`${css.line} ${menuOpen ? css.change : ''}`}></div>
+    <div class={`${css.line} ${menuOpen ? css.change : ''}`}></div>
+  </div>
+</nav>
