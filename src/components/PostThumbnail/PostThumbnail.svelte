d58adfb4a5542332b5b493ac822828df4305aa22
Andrea Bianchi <bianan96@gmail.com>
2024-09-26 22:57:23 +0200

    Fix component

--- src/components/PostThumbnail/PostThumbnail.svelte
+++ src/components/PostThumbnail/PostThumbnail.svelte
@@ -1,14 +1,22 @@
-<script>
+<script lang="ts">
   import { formatDate } from '../../utils/formatDate'
   import css from './PostThumbnail.module.scss'
 
-  export let post = {}
+  export let post: {
+    slug: string
+    data: {
+      title: string
+      description: string
+      date: Date
+      image: string
+    }
+  }
 </script>
 
 <div class={css.article}>
   <div class={css.container}>
     <div class={css.head}>
-      <time class={css.date} datetime={post.data.date}
+      <time class={css.date} datetime={post.data.date.toISOString()}
         >{formatDate(post.data.date)}
       </time>
       <a class={css.image} href={`/blog/${post.slug}`}>
