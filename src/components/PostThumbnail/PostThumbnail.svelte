d99c796ede2cc722d532808f0c06e42288895eeb
Andrea Bianchi <bianan96@gmail.com>
2024-09-27 12:40:09 +0200

    clean up project config

--- src/components/PostThumbnail/PostThumbnail.svelte
+++ src/components/PostThumbnail/PostThumbnail.svelte
@@ -1,24 +1,24 @@
 <script lang="ts">
-  import { formatDate } from '../../utils/formatDate'
-  import css from './PostThumbnail.module.scss'
+  import FormattedDate from '../FormattedDate/FormattedDate.svelte';
+  import css from './PostThumbnail.module.scss';
 
   export let post: {
-    slug: string
+    slug: string;
     data: {
-      title: string
-      description: string
-      date: Date
-      image: string
-    }
-  }
+      title: string;
+      description: string;
+      date: Date;
+      image: string;
+    };
+  };
 </script>
 
 <div class={css.article}>
   <div class={css.container}>
     <div class={css.head}>
-      <time class={css.date} datetime={post.data.date.toISOString()}
-        >{formatDate(post.data.date)}
-      </time>
+      <div class={css.date}>
+        <FormattedDate date={post.data.date} />
+      </div>
       <a class={css.image} href={`/blog/${post.slug}`}>
         <img src={post.data.image} alt={post.data.title} />
       </a>
--- src/lib/utils/kebabCase.ts
+++
@@ -1,6 +0,0 @@
-// // import { slug } from 'github-slugger';
-
-// // const kebabCase = (str: string) => slug(str);
-// const kebabCase = (str: string) => str;
-
-// export default kebabCase;
