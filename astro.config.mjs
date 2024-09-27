c76f5fe80a61eaffc94f4b6765de4dc887c4c490
Andrea Bianchi <bianan96@gmail.com>
2024-09-27 18:02:57 +0200

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,28 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import svelte from '@astrojs/svelte';
 import sitemap from '@astrojs/sitemap';
+import svelte from '@astrojs/svelte';
+import cloudflare from '@astrojs/cloudflare';
+import { remarkReadingTime } from './remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap(), svelte()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), svelte()],
+  output: 'server',
+  adapter: cloudflare({
+    runtime: {
+      mode: 'local',
+    },
+  }),
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern',
+      },
+    },
+  },
 });
