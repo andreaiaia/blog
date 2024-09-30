a8d1f8961aec770567d1607d98c7320b16ee1bf1
Andrea Bianchi <bianan96@gmail.com>
2024-09-30 18:23:16 +0200

    Add arrow icon to homepage

---
+++ src/components/Icons/ArrowDownCircleIcon.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { ArrowDownCircleIcon } from 'svelte-feather-icons';
+
+  export let size = '24';
+  export let addStyles = '';
+</script>
+
+<ArrowDownCircleIcon {size} class={addStyles} />
