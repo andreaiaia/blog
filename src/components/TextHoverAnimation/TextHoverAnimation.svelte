36e305505df96d80c5246336e7cc4713b7d888b8
Andrea Bianchi <bianan96@gmail.com>
2024-09-30 13:03:01 +0200

    fix css

---
+++ src/components/TextHoverAnimation/TextHoverAnimation.svelte
@@ -0,0 +1,10 @@
+<script>
+  import css from './TextHoverAnimation.module.scss';
+
+  export let text = '';
+  export let addStyle = '';
+</script>
+
+<p class={`${css.cta} ${addStyle}`} data-text={text}>
+  {text}
+</p>
