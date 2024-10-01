f436c3efbe558c25bd7269213e65cb08b4a66104
Andrea Bianchi <bianan96@gmail.com>
2024-10-01 20:09:43 +0200

    Set up content collection for galleries

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,16 +2,43 @@ import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
   type: 'content',
-  // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
     description: z.string(),
     tags: z.array(z.string()),
-    // Transform string to Date object
-    date: z.coerce.date(),
+    date: z.coerce.date(), // Transform string to Date object
     image: z.string().optional(),
     lang: z.string().optional(),
   }),
 });
 
-export const collections = { blog };
+const albums = defineCollection({
+  type: 'data',
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    cover: z.object({
+      w400: z.string(),
+      w800: z.string(),
+      w1024: z.string(),
+      w1600: z.string(),
+      w2560: z.string(),
+    }),
+    coverAlt: z.string().optional(),
+    images: z.array(
+      z.object({
+        srcset: z.object({
+          w400: z.string(),
+          w800: z.string(),
+          w1024: z.string(),
+          w1600: z.string(),
+          w2560: z.string(),
+        }),
+        description: z.string().optional(),
+        alt: z.string().optional(),
+      })
+    ),
+  }),
+});
+
+export const collections = { blog, albums };
