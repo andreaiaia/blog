70098a0d92a2eb3534668ce8b449dba2db56a8fe
Andrea Bianchi <bianan96@gmail.com>
2024-10-02 21:48:09 +0200

    add react for the gallery component

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import svelte from '@astrojs/svelte';
+import react from '@astrojs/react';
 import cloudflare from '@astrojs/cloudflare';
 import { remarkReadingTime } from './remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), svelte()],
+  integrations: [mdx(), sitemap(), svelte(), react()],
   output: 'hybrid',
   adapter: cloudflare(),
   markdown: {
