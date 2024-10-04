ebead6a4cc809ee061c3716956cc385c4e0e2cc1
Andrea Bianchi <bianan96@gmail.com>
2024-10-04 11:16:31 +0200

    Fix gallery layout on mobile

---
+++ src/components/Gallery/Gallery.svelte
@@ -0,0 +1,10 @@
+<script>
+  import css from './Gallery.module.scss';
+  export let images = [];
+</script>
+
+<div class={css.galleryGrid}>
+  {#each images as image}
+    <img src={image.srcset.w800} alt={image.alt} />
+  {/each}
+</div>
