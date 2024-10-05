d924fc7d92fbba64f6b31e94db1ad5d0b76c9f91
Andrea Bianchi <bianan96@gmail.com>
2024-10-05 15:00:57 +0200

    Fix gallery masonry layout with custom made solution

--- src/components/Gallery/Gallery.svelte
+++ src/components/Gallery/Gallery.svelte
@@ -3,8 +3,18 @@
   export let images = [];
 </script>
 
-<div class={css.galleryGrid}>
+<div class={css.gallery}>
   {#each images as image}
-    <img src={image.srcset.w800} alt={image.alt} />
+    <img
+      alt={image.alt}
+      src={image.srcset.w800}
+      srcset={`
+        ${image.srcset.w400} 400w,
+        ${image.srcset.w800} 800w,
+        ${image.srcset.w1024} 1024w,
+      `}
+      sizes="(max-width: 400px) 400px, (max-width: 800px) 800px, 1024px"
+      loading="lazy"
+    />
   {/each}
 </div>
