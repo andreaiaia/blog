cebb0dcfe6c57d74b58e206672fc4aba318098b4
Andrea Bianchi <bianan96@gmail.com>
2025-01-31 21:14:29 +0100

    remove react from project

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import svelte from '@astrojs/svelte';
-import react from '@astrojs/react';
 import cloudflare from '@astrojs/cloudflare';
 import { remarkReadingTime } from './remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), svelte(), react()],
+  integrations: [mdx(), sitemap(), svelte()],
   output: 'static',
   adapter: cloudflare(),
   markdown: {
